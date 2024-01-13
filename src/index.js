ea019a531da263361c9a30b216633349133873c1
Arush Choudhary <arushgzbin@gmail.com>
2024-01-13 21:07:18 +0530

    made user login route and solved some errors At this moment everything is working fine!!!!!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path:'./env'});
-import dotenv from "dotenv"
+import {app} from "./app.js";
 import connectDB from "./db/db.js";
+import dotenv from "dotenv";
 dotenv.config({
     path : "./env"
 });
